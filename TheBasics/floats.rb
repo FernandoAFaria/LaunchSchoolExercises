3132de4236031e73b4bc50c5975c2ed864cb8507
Fernando Faria <fernandoafaria@yahoo.com>
2018-08-02 13:03:52 -0400

    Variables done

---
+++ TheBasics/floats.rb
@@ -0,0 +1,7 @@
+def floats(num)
+  return num * num
+end
+
+puts floats(5)
+puts floats(15)
+puts floats(25)
---
+++ Variables/name.rb
@@ -0,0 +1,5 @@
+puts "Please enter your first name:"
+firstName = gets.chomp
+puts "Please enter your last name:"
+lastName = gets.chomp
+puts "Hello #{firstName} #{lastName}"
